f50cefe2f2da549a75dcad81ea908b6faefddf63
Roman Kartsev <chestozo@yandex-team.ru>
2017-10-21 15:13:13 +0300

    so we have a crashing button #1

--- index.js
+++ index.js
@@ -3,10 +3,38 @@ class App {
         [ 'stop', 'crashByMemory' ].forEach(method => this[method] = this[method].bind(this));
     }
 
+    init() {
+        // TODO buttons:
+        // infinite loop
+        // infinite ajax requests
+        // infinite ajax sync requests
+
+        this._onClick('#crash-by-memory', this.crashByMemory);
+        this._onClick('#crash-by-memory-fast', this.crashByMemoryFast);
+        this._onClick('#stop', this.stop);
+        return this;
+    }
+
+    /**
+     * - Chrome: no crash here after several minutes of run, just eating 2.3Gb of RAM on my Mac.
+     */
     crashByMemory() {
         let str = '';
         while(true) {
-            str += Math.random();
+            str += Math.random() + Math.random();
+        }
+    }
+
+    /**
+     * - Chrome: almost immediate crush with error "RangeError: Invalid string length".
+     * The same can be achieved with big object JSON.stringify().
+     */
+    crashByMemoryFast() {
+        let str = '';
+        let tmp;
+        while(true) {
+            tmp = str;
+            str += tmp + Math.random();
         }
     }
 
@@ -17,29 +45,6 @@ class App {
     _onClick(selector, handler) {
         document.querySelector(selector).addEventListener('click', handler);
     }
-
-    init() {
-        this._onClick('#stop', this.stop);
-        this._onClick('#crash-by-memory', this.crashByMemory);
-        return this;
-    }
 }
 
 window.app = new App().init();
-
-// // Buttons
-// memory
-// infinite loop
-// infinite ajax requests
-// infinite ajax sync requests
-
-// // try
-// sessionStorage
-// localStorage
-// cookie?
-
-// // crashReport
-// upTime (time passed from app start time)
-// memory? it is a bit difficult but possible
-// current url
-// prev url
